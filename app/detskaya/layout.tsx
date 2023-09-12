9d062fc9f7e0acac8bab9f5f28645bc66b96ad64
IntStrFloat <108020874+IntStrFloat@users.noreply.github.com>
2023-09-12 19:47:08 +0300

    Add metadata to all pages

--- app/detskaya/layout.tsx
+++ app/detskaya/layout.tsx
@@ -4,8 +4,8 @@ import { Provider } from '../provider';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Я пидорас',
-  description: 'Next App',
+  title: 'Детская мебель | Мебельщик тверь',
+  description: 'Детская мебель | Мебельщик тверь',
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
